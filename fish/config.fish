2d46c2b532e47a7902059de12b552e83b817b92f
crw <92102709+ChenRuiwei@users.noreply.github.com>
2023-12-11 18:04:38 +0800

    Fish: add starship

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,3 @@
-#!/usr/bin/fish
-
 abbr -a cat bat
 abbr -a j z
 abbr -a l "exa -ah"
@@ -10,16 +8,26 @@ abbr -a v nvim
 abbr -a g git
 abbr -a ga "git add"
 abbr -a gb "git branch"
-abbr -a gcam "git commit --all --message"
-abbr -a gcm "git commit"
-abbr -a gcmsg "git commit --message"
+abbr -a gc "git commit -v"
+abbr -a gca "git commit -va"
+abbr -a gca! "git commit -va --amend"
+abbr -a gcam "git commit -am"
+abbr -a gcmsg "git commit -m"
 abbr -a gco "git checkout"
 abbr -a gd "git diff"
 abbr -a gp "git push"
+abbr -a gr "git remote"
+abbr -a grv "git remote -v"
 abbr -a gs "git status"
 abbr -a gsw "git switch"
 abbr -a gloga "git log --all --graph --oneline --decorate"
 
 set fish_greeting
 
+{{#if (is_executable "starship")}}
+starship init fish | source
+{{/if}}
+
+{{#if (is_executable "zoxide")}}
 zoxide init fish | source
+{{/if}}
