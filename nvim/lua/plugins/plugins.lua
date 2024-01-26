955d9f08f19b293a3ebb856dfac1cab87550acb6
crw <92102709+ChenRuiwei@users.noreply.github.com>
2024-01-26 13:25:47 +0800

    Nvim: fix pangu.nvim filetype to markdown

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -122,11 +122,32 @@ return {
   {
     "kaarmu/typst.vim",
     ft = "typst",
-    lazy = false,
   },
 
   {
     "hotoo/pangu.vim",
-    ft = { "typst", "md" },
+    ft = { "typst", "markdown" },
+  },
+
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- telescope 所需
+      "MunifTanjim/nui.nvim",
+
+      -- 可选
+      "nvim-treesitter/nvim-treesitter",
+      "rcarriga/nvim-notify",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      -- 配置放在这里
+      lang = "rust",
+      cn = {
+        enabled = true,
+      },
+    },
   },
 }
