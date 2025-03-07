2cfdee6dc7763cb88dbd883a8fa4d24ec754bc98
ChenRuiwei <1982833213@qq.com>
2025-03-08 01:45:10 +0800

    scripts: update switch-wallpaper.sh by using wal to gen colors

--- scripts/switch-wallpaper.sh
+++ scripts/switch-wallpaper.sh
@@ -4,4 +4,4 @@ loaded_wallpapers="$(hyprctl hyprpaper listloaded)"
 selected_wallpaper="$(echo "$loaded_wallpapers" | rofi -dmenu)"
 
 hyprctl hyprpaper wallpaper ,"$selected_wallpaper"
-wpg -a "$selected_wallpaper" && wpg -A "$selected_wallpaper" && wpg -s "$selected_wallpaper"
+wal -i "$selected_wallpaper"
