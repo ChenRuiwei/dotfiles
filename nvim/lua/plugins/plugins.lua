6fcb5538de828961d70ee9c088aaf1dc763e76bb
ChenRuiwei <1982833213@qq.com>
2025-11-18 11:14:37 +0800

    nvim: remove leetcode.nvim due to noice conflict with nvim-notify

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -80,27 +80,26 @@ return {
     ft = { "typst", "markdown" },
   },
 
-  {
-    "kawre/leetcode.nvim",
-    build = ":TSUpdate html",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-      "nvim-lua/plenary.nvim", -- telescope 所需
-      "MunifTanjim/nui.nvim",
-
-      -- 可选
-      "nvim-treesitter/nvim-treesitter",
-      "rcarriga/nvim-notify",
-      "nvim-tree/nvim-web-devicons",
-    },
-    opts = {
-      -- 配置放在这里
-      lang = "cpp",
-      cn = {
-        enabled = true,
-      },
-    },
-  },
+  -- {
+  --   "kawre/leetcode.nvim",
+  --   build = ":TSUpdate html",
+  --   dependencies = {
+  --     "nvim-telescope/telescope.nvim",
+  --     "nvim-lua/plenary.nvim", -- telescope 所需
+  --     "MunifTanjim/nui.nvim",
+  --
+  --     -- 可选
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "nvim-tree/nvim-web-devicons",
+  --   },
+  --   opts = {
+  --     -- 配置放在这里
+  --     lang = "cpp",
+  --     cn = {
+  --       enabled = true,
+  --     },
+  --   },
+  -- },
 
   ---@type LazySpec
   {
